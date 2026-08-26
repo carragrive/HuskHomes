90eed66a6dca99b5b598368236f1b4077233816e
CarraGrive <50628910+CarraGrive@users.noreply.github.com>
2026-08-26 19:10:08 +0200

    rtp command executed via the console don't check player permissions anymore

--- common/src/main/java/net/william278/huskhomes/command/RtpCommand.java
+++ common/src/main/java/net/william278/huskhomes/command/RtpCommand.java
@@ -167,9 +167,9 @@ public class RtpCommand extends Command implements UserListTabCompletable {
             return Optional.empty();
         }
 
-        // Users can always RTP within the world and server they're currently on
-        if (!worldName.equals(teleporter.getPosition().getWorld().getName()) ||
-                (targetServer != null && !teleporter.getPosition().getServer().equals(targetServer))) {
+        // Console executions bypass the target player's destination permissions
+        if (executor instanceof OnlineUser && (!worldName.equals(teleporter.getPosition().getWorld().getName()) ||
+                (targetServer != null && !teleporter.getPosition().getServer().equals(targetServer)))) {
             if (!worldName.equals(teleporter.getPosition().getWorld().getName()) &&
                     !teleporter.hasPermission(getPermission(worldName))) {
                 plugin.getLocales().getLocale("error_no_permission")
